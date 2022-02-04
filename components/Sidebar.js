67f7b0740470b2558ad4e286c954044a2cad1fae
amangit1314 <gitaman8481@gmail.com>
2022-02-04 15:48:54 +0530

    whatsapp-clone

---
+++ components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Sidebar.css"
+function Sidebar(params) {
+    return <div className="sidebar">
+      <h1>Sidebar</h1>
+  </div>
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUn6VjqdrfYisu72Oy4LvQLGkQNB_8OZM',
+  authDomain: 'whatsapp-clone-clever-01-2022.firebaseapp.com',
+  projectId: 'whatsapp-clone-clever-01-2022',
+  storageBucket: 'whatsapp-clone-clever-01-2022.appspot.com',
+  messagingSenderId: '194328596433',
+  appId: '1:194328596433:web:ab5118df53d1e3a6835af0',
+  measurementId: 'G-0EKHJW9P80',
+}
